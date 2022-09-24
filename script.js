3951f389ea0559f01498258428a6f6700b70e39f
Rudra Vengurlekar <107509068+QuantRV@users.noreply.github.com>
2022-09-25 03:05:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // document.querySelector('iframe').contentWindow.document.querySelector('.ytp-chrome-top, .ytp-show-cards-title').style.visibility = "hidden";
 
-function preloaderFunction(){
+function preloaderFunction() {
     document.querySelector(".Rpreload").style.display = "none";
 }
 
-function removeTheme(){
+function removeTheme() {
     document.querySelector(".theme").style.display = "none";
 }
 
@@ -15,32 +15,32 @@ function myFunction() {
     console.log("hii");
     setTimeout(() => {
         removeTheme();
-    }, 78000);
-    }
+    }, 33500);
+}
 
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
 
- tag.src = "https://www.youtube.com/iframe_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
- var player;
- function onYouTubeIframeAPIReady() {
-   player = new YT.Player('player', {
-     width: '100%',
-     videoId: 'TrqNVXHHJFQ',
-     playerVars: { 'autoplay': 1, 'playsinline': 1 },
-     events: {
-       'onReady': onPlayerReady
-     }
-   });
- }
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        width: '100%',
+        videoId: 'TrqNVXHHJFQ',
+        playerVars: { 'autoplay': 1, 'playsinline': 1, 'start': 45 },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
 
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
     event.target.mute();
-   event.target.playVideo();
- }
+    event.target.playVideo();
+}
